56453a11fb9640fd3cbcf2aff62e72243d40afe9
DemiusLs <dloss320@gmail.com>
2025-11-11 23:18:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+    <main>
+        <section >
+            <div class="container text-center">
+
+            <h1 class="pt-4">The alchemist's Garden</h1>
+            <h2>Explore the arcane botany of healing</h2>
+            <h3>Benvenuti nel giardino dell'alchmista</h3>    
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum exercitationem expedita amet minima architecto,
+                 aliquid, quasi enim cumque, odio nesciunt sed vero. Aperiam eaque accusantium temporibus. Obcaecati delectus enim illum!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum exercitationem expedita amet minima architecto,
+                 aliquid, quasi enim cumque, odio nesciunt sed vero. Aperiam eaque accusantium temporibus. Obcaecati delectus enim illum!
+            </p>
+            
+
+            </div>
+            
+        </section>       
+        
+
+    </main>
+    </>
+  )
+}
+
+export default App
